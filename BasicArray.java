ce59ca8b2c98881c5b1a1a8cdc0c6d12c73e88f5
Rohith Gadde <50209060+RohithGadde@users.noreply.github.com>
2020-08-25 08:54:12 +0530

    Add files via upload

---
+++ BasicArray.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class BasicArray
+{
+ public static void main(String[] args)
+{
+  int a[] = new int[5];
+  for(int i=4;i<5;i++)
+  {
+   System.out.println(a[i]);
+  }
+ }
+}
